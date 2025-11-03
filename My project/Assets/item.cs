2aa364e89f404aa72ce98d94aad97b656a9fcb57
holafffffeeeeee <122540557+holafffffeeeeee@users.noreply.github.com>
2025-11-03 15:09:06 +0100

    initial commit

---
+++ My project/Assets/item.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class item 
+{
+
+    public string itemName;
+    public Sprite icon;
+    public bool isConsumable;
+    public string itemDescription;
+    public float effectValue;
+}
---
+++ My project/Assets/playercontoller.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playercontoller : MonoBehaviour
+{
+    [SerializeField] public float moveSpeed = 1f;
+    public InputActionAsset inputActions;
+   private Vector2 move;
+    private Rigidbody2D rb;
+    public void OnMove(InputAction.CallbackContext context)
+    {
+
+        // Read the movement input
+        move = context.ReadValue<Vector2>();
+        Debug.Log("moveing");
+    }
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+      
+    }
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + move * (moveSpeed * Time.fixedDeltaTime));
+    }
+}
