2b7dc3729a1a5c4b3f1222acba59121e63ab1493
Uthredthecat <gabriel.wikman@adelfors.nu>
2025-11-21 11:08:27 +0100

    added timer to bullets

--- My project/Assets/script/bullet.cs
+++ My project/Assets/script/bullet.cs
@@ -5,4 +5,11 @@ public class Bullet : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int shooterID;
     public float damage = 1f;
+    float lifeTime = 1f;
+
+    public void Awake() 
+    { 
+        
+        Destroy(gameObject, lifeTime); 
+    }
 }
