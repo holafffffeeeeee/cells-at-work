cbff31869573f29c11cdc0c111d03485c6bac0bb
Uthredthecat <gabriel.wikman@adelfors.nu>
2025-11-25 09:57:03 +0100

    Added sounds

--- My project/Assets/playerManager.cs
+++ My project/Assets/playerManager.cs
@@ -1,16 +1,15 @@
-using System.Collections.Generic;
+ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class playerManager : MonoBehaviour
 {
-  public  GameManager gameManager;
     public GameObject canvas;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
    public List<PlayerInput> players = new List<PlayerInput>();
 
     [SerializeField] private GameObject player2Prefab;
-   public LevelManager levelManager;
+
 
    public PlayerInputManager pim;
     public void onJoin(PlayerInput player)
@@ -28,10 +27,7 @@ public class playerManager : MonoBehaviour
     public void OnCanvas()
     {
         canvas.SetActive(false);
-      levelManager.Deathscreen.SetActive(false);
         Time.timeScale = 1;
-        gameManager.ScoreTextright.SetActive(true);
-        gameManager.ScoreTextleft.SetActive(true);
     }
     private static Color GetRandomColor()
     {
