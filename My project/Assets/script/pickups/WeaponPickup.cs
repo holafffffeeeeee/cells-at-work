8398541bdd271a574592ddd562cb1bd35cf3ba07
Uthredthecat <gabriel.wikman@adelfors.nu>
2025-11-26 09:34:47 +0100

    PickupWeaponsound

--- My project/Assets/script/pickups/WeaponPickup.cs
+++ My project/Assets/script/pickups/WeaponPickup.cs
@@ -1,26 +1,37 @@
+using System.Threading;
 using UnityEngine;
 
 public class WeaponPickup : MonoBehaviour
 {
+    public AudioSource Audioplayer;
+    public AudioClip PickupWeapon;
     [SerializeField] private WeaponData weaponData;
 
     shooting bu;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        
         if (other.gameObject.tag == "Player")
         {
             shooting playerShooting = other.GetComponent<shooting>();
 
+            Audioplayer.PlayOneShot(PickupWeapon);
+            
             if (playerShooting != null)
             {
+               
                 playerShooting.SetWeaponStats(
                     weaponData.fireRate,
                     weaponData.damage,
                     weaponData.bulletSpeed
                 );
 
-                Destroy(gameObject);
+
+
+            Destroy (gameObject,PickupWeapon.length);
+
+                
             }
         }
     }
