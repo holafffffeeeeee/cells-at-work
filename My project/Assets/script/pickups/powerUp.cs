d515f7bbb93bdc3b2e30511e4be1f7882bbbb856
Uthredthecat <gabriel.wikman@adelfors.nu>
2025-11-29 14:49:45 +0100

    Deed sound to pickupHealth

--- My project/Assets/script/pickups/powerUp.cs
+++ My project/Assets/script/pickups/powerUp.cs
@@ -2,14 +2,17 @@ using UnityEngine;
 
 public class powerUp : MonoBehaviour
 {
+    public AudioSource Audioplayer;
+    public AudioClip PickupHealth;
     public PowerupEffect powerUpEffect;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Audioplayer.PlayOneShot(PickupHealth);
         if ( other.gameObject.tag == "Player")
         {
-            Destroy(gameObject);
+            Destroy(gameObject,PickupHealth.length);
             powerUpEffect.apply(other.gameObject);
         }
     }
